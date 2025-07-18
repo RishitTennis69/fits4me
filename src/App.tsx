0d225adb30864d374ee1ffd8eac1222ec680a199
Krish Grover <krish.s.grover@gmail.com>
2025-07-18 07:05:32 -0700

    Set / as Landing page, /app as Try It On page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import Dashboard from './pages/Dashboard';
 import Wardrobe from './pages/Wardrobe';
+import Landing from './pages/Landing';
 import { Toaster } from '@/components/ui/toaster';
 import './App.css';
 
@@ -10,9 +11,10 @@ function App() {
     <Router>
       <div className="App">
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Landing />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/wardrobe" element={<Wardrobe />} />
+          <Route path="/app" element={<Index />} />
         </Routes>
         <Toaster />
       </div>
